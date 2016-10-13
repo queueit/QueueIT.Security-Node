33f59555ffd969e8f0cab697156bbfaae40edb4c
Morten Brix Pedersen <mope@queue-it.com>
2016-10-13 10:39:41 +0200

    No longer require queueDomain as option.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var queueit = require('./lib/queueit.js');
 var options = {
   customerId: "ticketania", // Customer id
   eventId: "link", // Event id
-  queueDomain: "http://ticketania.queue-it.net/", // Queue url from GO platform
   defaultKnownUserSecretKey: "a774b1e2-8da7-4d51-b1a9-7647147bb13bace77210-a488-4b6f-afc9-8ba94551a7d7" // Secret key from queue-it account
 };
 var queue = queueit(options);
